dfef45f638ace23e35af1c17a0974fef76832bf2
dmatviichuk <matviichuk.dmytro@gmail.com>
2021-01-05 22:33:51 +0100

    Added searchGroups

--- src/Enrollment.java
+++ src/Enrollment.java
@@ -4,6 +4,14 @@ import java.util.List;
 public class Enrollment {
     public List<Student> studentList = new ArrayList<>();
     public List<Groups> groupsList = new ArrayList<>();
+    public List<Enrollment> enrollmentList = new ArrayList<>();
 
-
+    private Groups searchGroups(Groups groups) {
+        if(groups != null)
+            for(int i = 0; i < groupsList.size(); i++){
+                if(groupsList.get(i).equals(groups))
+                    return groupsList.get(i);
+            }
+        return null;
+    }
 }
