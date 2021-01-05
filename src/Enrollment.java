742dd036d158ea159654d87a83af9049c60a5d97
dmatviichuk <matviichuk.dmytro@gmail.com>
2021-01-05 22:10:07 +0100

    Some changes

--- src/Enrollment.java
+++ src/Enrollment.java
@@ -1,6 +1,7 @@
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class Enrollment {
-
-
+    public List<Student> studentList = new ArrayList<>();
+    public List<Groups> groupsList = new ArrayList<>();
 }
